7b8fb5ae726997d6bcecdf22b8f02804c2777be8
AdityaBornare <98082615+AdityaBornare@users.noreply.github.com>
2022-06-03 01:45:51 +0530

    Add files via upload

--- lib/src/settings_page.dart
+++ lib/src/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'App_info.dart' as App_info;
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,9 +17,78 @@ class HomePage extends StatelessWidget {
               },
               icon: const Icon(Icons.menu),
             ),
+            
           ],
+        ),
+        body: Center(
+          child: Column(
+            children:<Widget>[
+              Container(child: MyStatefulWidget()),
+              Container(
+              color: Colors.white,
+              height: 50,
+              width: 100,
+            ),
+              Container(child: FlatButton(
+                color: Color.fromARGB(255, 26, 192, 32),
+                textColor: Colors.white,
+                onPressed: () {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const App_info.Infopage()),
+                );
+
+                },
+                child: Text('App Info'),
+                ),
+                ),
+                
+            ],
+          ),
+          
+          
         ),
       ),
     );
   }
 }
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'Settings';
+
+  @override
+  Widget build(BuildContext context) {
+    DecoratedBox(decoration: BoxDecoration(color: Colors.green),);    
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.add),
+      elevation: 8,
+      style: const TextStyle(color: Colors.green),
+      underline: Container(
+        height: 4,
+        color: Color.fromARGB(255, 45, 212, 51),
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['Settings','user','log in','log out']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
