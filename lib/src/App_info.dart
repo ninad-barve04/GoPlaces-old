c987bd58b9ca33b3cefd306029afcb76edfb41a7
AdityaBornare <98082615+AdityaBornare@users.noreply.github.com>
2022-06-03 01:46:16 +0530

    Add files via upload

---
+++ lib/src/App_info.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Infopage extends StatelessWidget {
+  const Infopage({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("App Info"),
+          backgroundColor: const Color.fromARGB(255, 8, 214, 118),
+          actions: [
+            BackButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child:Text('                                                                WELCOME TO VisitHere APP.\n\n\nThis app is a perfect place to find your destination and explore the surroundings.Just let us know your location and we provide you suggestions of all exciting places near by with lots of images and info for you to choose from them the place to go.\n Login from your google account to create a user profile and you are ready to go. ',style: TextStyle(fontSize: 25,color: Color.fromARGB(255, 11, 126, 14),fontStyle: FontStyle.italic,wordSpacing: 10,shadows:[Shadow(color: Colors.yellowAccent,blurRadius: 15)]),),
+
+        ),
+      ),
+    );
+  }
+}
