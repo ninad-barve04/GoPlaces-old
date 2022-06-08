644fc6b19ef4c511619f722b3f0d7a4f531d7764
AdityaBornare <98082615+AdityaBornare@users.noreply.github.com>
2022-06-08 21:16:36 +0530

    Add files via upload

--- lib/page/main_map_page.dart
+++ lib/page/main_map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../src/locations.dart' as locations;
 import 'info_panel.dart' as infopanel;
@@ -39,6 +40,11 @@ import 'app_info.dart' as appinfo;
 //     );
 //   }
 // }
+final GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: [
+    'email'
+  ]
+);
 
 class MapState extends StatefulWidget {
   const MapState({Key? key}) : super(key: key);
@@ -92,12 +98,27 @@ class _MapState extends State<MapState> {
     });
   }
 
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState(){
+    _googleSignIn.onCurrentUserChanged.listen((account) {
+      setState(() {
+        _currentUser = account;
+      });
+    });
+    _googleSignIn.signInSilently();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     BorderRadiusGeometry radius = const BorderRadius.only(
       topLeft: Radius.circular(30),
       topRight: Radius.circular(30),
     );
+    GoogleSignInAccount? user = _currentUser;
+    if(user !=null){
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
@@ -121,59 +142,165 @@ class _MapState extends State<MapState> {
         ),
       ),
       drawer: Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage('assets/images/drawer.jpg'),
-                ),
-              ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:<Widget>[
+              DrawerHeader(decoration: BoxDecoration(image: DecorationImage(fit: BoxFit.fill,image: AssetImage('assets/images/drawer.jpg'),),),
               child: Container(
-                child: Column(
-                  children: const [
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CircleAvatar(
-                      radius: 42,
-                      backgroundImage: AssetImage('assets/images/user.jpg'),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                  ],
+                child: Column(children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  CircleAvatar(
+                    radius: 42,
+                    backgroundImage: AssetImage('assets/images/user.jpg'),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
                 ),
               ),
-            ),
-            Container(child: const MyStatefulWidget()),
-            Container(
+              ),
+              Container(
+                child: Column(
+                children: [
+                  ListTile(
+                    leading: GoogleUserCircleAvatar(identity: user),
+                    title: Text(user.displayName ?? ''),
+                    subtitle: Text(user.email),
+                  ),
+                  const SizedBox(height: 10,),
+                  ElevatedButton(
+                    onPressed: signOut, 
+                    child: const Text('Sign Out')),
+                ],
+              )),
+              Container(
               color: Colors.white,
               height: 50,
               width: 100,
             ),
-            Container(
-              child: FlatButton(
-                color: const Color.fromARGB(255, 0, 92, 179),
+              Container(child: FlatButton(
+                color: Color.fromARGB(255, 26, 192, 32),
                 textColor: Colors.white,
                 onPressed: () {
                   Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const appinfo.Infopage()),
-                  );
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const appinfo.Infopage()),
+                );
+
                 },
-                child: const Text('App Info'),
-              ),
-            ),
-          ],
+                child: Text('App Info'),
+                ),
+                ),
+                
+            ],
+          ),
         ),
-      ),
     ));
   }
+  else{
+    return MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
+        title: const Text("GoPlaces..."),
+        backgroundColor: const Color.fromARGB(255, 0, 92, 179),
+      ),
+      body: SlidingUpPanel(
+        controller: _pc,
+        parallaxEnabled: true,
+        parallaxOffset: 0.5,
+        minHeight: 0,
+        panel: infopanel.InfoPanelLayout(currentPoi),
+        borderRadius: radius,
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(18.5293825, 73.8543523),
+            zoom: 12.0,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+      ),
+        
+        drawer: Drawer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:<Widget>[
+              DrawerHeader(decoration: BoxDecoration(image: DecorationImage(fit: BoxFit.fill,image: AssetImage('assets/images/drawer.jpg'),),),
+              child: Container(
+                child: Column(children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  CircleAvatar(
+                    radius: 42,
+                    backgroundImage: AssetImage('assets/images/user.jpg'),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+                ),
+              ),
+              ),
+              Container(
+                child: Column(
+                children: [
+                  const SizedBox(height: 20,),
+                  const Text(
+                    'Not signed in',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(height: 10,),
+                  ElevatedButton(
+                    onPressed: signIn, 
+                    child: const Text('Sign In')),
+                ],
+              )),
+              Container(
+              color: Colors.white,
+              height: 50,
+              width: 100,
+            ),
+              Container(child: FlatButton(
+                color: Color.fromARGB(255, 26, 192, 32),
+                textColor: Colors.white,
+                onPressed: () {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const appinfo.Infopage()),
+                );
+
+                },
+                child: Text('App Info'),
+                ),
+                ),
+                
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
+}
+
+void signOut(){
+    _googleSignIn.disconnect();
+  }
+
+  Future<void> signIn() async{
+    try{
+      await _googleSignIn.signIn();
+    }
+    catch(e){
+      print('Error in signIn $e');
+    }
+  }
 
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
